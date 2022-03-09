e94270ebada85ec33ab01e8f642bca88a1aece3a
Pedro Donaldo Sanchez Galvan <pd.sanchezgalvan@ugto.mx>
2022-03-08 22:05:03 -0600

    app.js new comment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! My name is Pedro and this is my first example in AWS but it doesnt let me modify the files'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
